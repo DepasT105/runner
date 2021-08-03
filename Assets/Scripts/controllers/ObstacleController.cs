4058b4db42c7fd8e8f39a1cae533ef1efdd33819
Thomas <depat105@gmail.com>
2021-08-03 15:57:32 -0400

    Initial commit

---
+++ Assets/Scripts/controllers/ObstacleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+
+
+    public GameObject brokenObj;
+    GameObject thisObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisObj = GetComponent<GameObject>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject obj = collision.gameObject;
+        if (obj.tag == "Player")
+        {
+            Transform oldTrans = obj.transform;
+            Destroy(thisObj);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/controllers/SpecialController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialController : MonoBehaviour {
+    Rigidbody rb;
+
+    private float specPosX = 0;
+    public float timeBeforeNextSpecial = 5.0f;
+    public float distanceTraveled = 25;
+    public float specialSpeed = 5000;
+    private float canShoot = 0f;
+    // Start is called before the first frame update
+    void Start () {
+    }
+
+     void OnCollisionEnter (Collision collision) {
+    GameObject obj = collision.gameObject;
+    if (obj.tag == "Obstacle") {
+     Debug.Log("man down");
+    }
+    
+  }
+}
